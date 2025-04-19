f41b1ef09c380df89bc1ff24bfda8bf87ba1c910
makim0939 <95673492+makim0939@users.noreply.github.com>
2025-04-19 17:13:05 +0900

    feat: サイト内のテキストを扱うTextコンポーネントを作成。

---
+++ src/components/ui/Text.tsx
@@ -0,0 +1,48 @@
+import { twMerge } from "tailwind-merge";
+import { tv } from "tailwind-variants";
+
+type TextProps = {
+	variant?: "h1" | "h2" | "h3" | "p" | "small";
+	children: React.ReactNode;
+} & (
+	| React.HTMLAttributes<HTMLHeadingElement>
+	| React.HTMLAttributes<HTMLParagraphElement>
+);
+
+const text = tv({
+	base: " text-maki-black ",
+	variants: {
+		variant: {
+			h1: " text-3xl md:text-5xl 2xl:text-[64px] font-semibold",
+			h2: " text-2xl md:text-[40px] font-semibold",
+			h3: " text-xl md:text-4xl font-semibold",
+			p: " text-base md:text-xl leading-8 md:leading-12",
+			small: " text-sm md:text-base text-maki-gray  ",
+		},
+	},
+	compoundVariants: [
+		{
+			variant: ["h1", "h2", "h3"],
+			className: " tracking-[0.21em] ",
+		},
+		{
+			variant: ["p", "small"],
+			className: " tracking-[0.06em] ",
+		},
+	],
+	defaultVariants: {
+		variant: "p",
+	},
+});
+
+function Text({ variant = "p", children, className, ...props }: TextProps) {
+	const Tag = variant;
+
+	return (
+		<Tag className={twMerge(text({ variant }), className)} {...props}>
+			{children}
+		</Tag>
+	);
+}
+
+export default Text;
