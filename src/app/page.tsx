6fcaa4975fed87a91e266eaa7a4c3cfb74ebaef6
makim0939 <95673492+makim0939@users.noreply.github.com>
2025-04-19 17:13:43 +0900

    feat: Textコンポーネントを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
+import Text from "@/components/ui/Text";
 import Link from "next/link";
 import Scene from "../components/3d/Scene";
 
 export default function Home() {
 	return (
 		<div className="flex flex-col w-full h-full">
-			<section className="px-8 pt-16 md:w-2xl md:p-16">
+			<div className="px-8 pt-16 md:w-2xl md:p-16">
 				<header className=" md:mb-12 [&>p]:ml-1 [&>p]:md:ml-1.5">
-					<h1 className="mb-1 text-4xl font-bold leading-relaxed md:text-5xl ">
-						まきむらは、
+					<Text variant="h1" className=" my-4">
+						まきむら<span className=" text-lg md:text-4xl">の</span>
 						<br />
-						<span className=" dot-text">制作</span>中...
-					</h1>
-					<p>まきむらのポートフォリオサイト。</p>
+						ポートフォリオ
+					</Text>
+					<Text>
+						<span className=" md:text-3xl tracking-[.21em]">
+							こんにちは。まきむらです！
+						</span>
+					</Text>
 				</header>
 				<main>
 					<section>
-						<p>
+						<Text>
+							<br />
 							このサイトは現在制作中です。
 							<br />
 							5月末までに、初期リリース版を公開予定です。
@@ -29,10 +35,10 @@ export default function Home() {
 								</span>
 								→
 							</a>
-						</p>
+						</Text>
 					</section>
 				</main>
-			</section>
+			</div>
 			<Scene />
 		</div>
 	);
