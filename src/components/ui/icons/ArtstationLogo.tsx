4d94c4af379e17540bf9bb7c2628f0fad798bc10
makim0939 <95673492+makim0939@users.noreply.github.com>
2025-04-26 09:11:27 +0900

    feat: Logoコンポーネントの作成

---
+++ src/components/ui/icons/ArtstationLogo.tsx
@@ -0,0 +1,30 @@
+type SNSLogoProps = React.SVGAttributes<SVGElement>;
+function ArtstationLogo({ fill, ...props }: SNSLogoProps) {
+	return (
+		<svg
+			width="32"
+			height="32"
+			viewBox="0 0 32 32"
+			fill={fill}
+			xmlns="http://www.w3.org/2000/svg"
+			role="img"
+			aria-label="X logo"
+			{...props}
+		>
+			<path
+				d="M3 21.7806L5.18715 25.5943C5.62949 26.4611 6.51418 27.0554 7.52174 27.0554H22.0945L19.0718 21.7806H3Z"
+				fill={fill}
+			/>
+			<path
+				d="M28.9998 21.8055C28.9998 21.2854 28.8523 20.7901 28.582 20.3691L20.03 5.41156C19.5877 4.56958 18.7275 4 17.72 4H13.1982L26.3948 27.0307L28.4837 23.3904C28.8769 22.697 28.9998 22.3998 28.9998 21.8055Z"
+				fill={fill}
+			/>
+			<path
+				d="M16.9334 18.0164L11.06 7.73926L5.16211 18.0164H16.9334Z"
+				fill={fill}
+			/>
+		</svg>
+	);
+}
+
+export default ArtstationLogo;
---
+++ src/components/ui/icons/XLogo.tsx
@@ -0,0 +1,22 @@
+type SNSLogoProps = React.SVGAttributes<SVGElement>;
+function XLogo({ fill, ...props }: SNSLogoProps) {
+	return (
+		<svg
+			width="32"
+			height="32"
+			viewBox="0 0 32 32"
+			fill={fill}
+			xmlns="http://www.w3.org/2000/svg"
+			role="img"
+			aria-label="X logo"
+			{...props}
+		>
+			<path
+				d="M17.9209 14.0092L27.2543 3H25.0426L16.9384 12.5592L10.4656 3H3L12.7881 17.4551L3 29H5.21184L13.7701 18.9052L20.6058 29H28.0714L17.9204 14.0092H17.9209ZM14.8915 17.5825L13.8997 16.1431L6.0088 4.68958H9.40606L15.7741 13.9329L16.7659 15.3723L25.0436 27.3873H21.6464L14.8915 17.583V17.5825Z"
+				fill={fill}
+			/>
+		</svg>
+	);
+}
+
+export default XLogo;
