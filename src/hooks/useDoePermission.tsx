82b20558fed7c015dbb5427b37528112c57f60c0
makim0939 <makim0939@gmail.com>
2025-09-28 16:46:07 +0900

    fix: DeviceOrientationの許可の取り方を変更。許可ボタン周りの見た目を整えた

--- src/hooks/useDoePermission.tsx
+++ src/hooks/useDoePermission.tsx
@@ -1,39 +1,48 @@
 import { useCallback, useEffect, useState } from "react";
 
+export type PermissionResultType =
+	| "granted"
+	| "denied"
+	| "default"
+	| "notSupported";
+
 export function useDoePermission() {
-	const [doePermission, setDoePermission] = useState(false);
+	const [doePermission, setDoePermission] =
+		useState<PermissionResultType | null>(null);
 
 	const checkDoePermission = useCallback(() => {
 		const DOE = DeviceOrientationEvent as typeof DeviceOrientationEvent & {
-			requestPermission: () => Promise<"granted" | "denied" | "default">;
+			requestPermission: () => Promise<PermissionResultType>;
 		};
-		DOE.requestPermission().then(async (val: string) => {
-			if (val === "granted") {
-				setDoePermission(true);
-			} else {
-				setDoePermission(false);
-			}
-		});
+		DOE.requestPermission()
+			.then((val: PermissionResultType) => {
+				setDoePermission(val);
+			})
+			.catch(() => {
+				setDoePermission("default");
+			});
 	}, []);
 
 	useEffect(() => {
-		if (!Object.hasOwn(DeviceOrientationEvent, "requestPermission")) return;
-		checkDoePermission();
+		// DeviceOrientationに対応しているかどうかをチェックする。
+		if (
+			typeof window === "undefined" ||
+			!("DeviceOrientationEvent" in window) ||
+			!Object.hasOwn(window.DeviceOrientationEvent, "requestPermission")
+		) {
+			setDoePermission("notSupported");
+		} else {
+			checkDoePermission();
+		}
 	}, [checkDoePermission]);
 
-	// requestPermissionが使えない場合は、nullを返す
-	if (
-		typeof window === "undefined" ||
-		!("DeviceOrientationEvent" in window) ||
-		!Object.hasOwn(window.DeviceOrientationEvent, "requestPermission")
-	) {
+	// DeviceOrientationEventに未対応の場合、checkDoePermissionは使えないようにする。
+	if (doePermission === "notSupported")
 		return {
-			doePermission: null,
+			doePermission,
 			checkDoePermission: () => {
 				console.warn("requestPermission is not supported");
 			},
 		};
-	}
-
 	return { doePermission, checkDoePermission };
 }
