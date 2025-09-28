f0b8f6b31a26ddd0bb52802dbb5660b2a4333542
makim0939 <makim0939@gmail.com>
2025-09-28 11:20:37 +0900

    feat: 子要素に対して順に出現アニメーションをするコンポーネントを作成

---
+++ src/components/ui/FadeInContainer.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { type ReactNode, useEffect, useRef, useState } from "react";
+
+type FadeInContainerProps = {
+	children: ReactNode;
+	distance?: number;
+	duration?: number;
+	childrenDelay?: number;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function FadeInContainer({
+	children,
+	distance = 16,
+	duration = 250,
+	childrenDelay = 100,
+	...props
+}: FadeInContainerProps) {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const [visibleIndexes, setVisibleIndexes] = useState<Set<number>>(new Set());
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					const index = Number(entry.target.getAttribute("data-index"));
+					if (entry.isIntersecting) {
+						setVisibleIndexes((prev) => new Set(prev).add(index));
+					}
+				});
+			},
+			{ threshold: 0.2 },
+		);
+
+		const childrenElements = containerRef.current?.children;
+		if (childrenElements) {
+			Array.from(childrenElements).forEach((child, index) => {
+				child.setAttribute("data-index", index.toString());
+				observer.observe(child);
+			});
+		}
+
+		return () => {
+			if (childrenElements) {
+				Array.from(childrenElements).forEach((child, _) =>
+					observer.unobserve(child),
+				);
+			}
+		};
+	}, []);
+
+	const childArray = React.Children.toArray(children);
+
+	return (
+		<div ref={containerRef} {...props}>
+			{childArray.map((child, index) => (
+				<div
+					key={index}
+					style={{
+						opacity: visibleIndexes.has(index) ? 100 : 0,
+						transform: visibleIndexes.has(index)
+							? "translateY(0)"
+							: `translateY(${distance}px)`,
+						transitionDelay: `${index * childrenDelay}ms`,
+						transitionDuration: `${duration}ms`,
+						transitionTimingFunction: "ease-out",
+					}}
+				>
+					{child}
+				</div>
+			))}
+		</div>
+	);
+}
