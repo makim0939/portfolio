ef405ccbcfa6fdff74d629eb60937a9b886ff983
makim0939 <makim0939@gmail.com>
2025-09-28 23:54:12 +0900

    fix: 成果物の詳細ページで、カバーで動画を流せるように変更。

--- src/app/works/[slug]/page.tsx
+++ src/app/works/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { MDXContent } from "@/components/ui/MdxContent";
 import { Text } from "@/components/ui/Text";
 import { getAllWorks, getWorkBySlug } from "@/lib/works";
+import { FadeInContainer } from "@/components/ui/FadeInContainer";
 
 export async function generateStaticParams() {
 	const works = await getAllWorks();
@@ -22,17 +23,33 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
 export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
 	const { slug } = await params;
 	const work = await getWorkBySlug(slug);
+	const isCoverVideo = work?.coverImage.split(".")[1] === "mp4";
 	if (!work) return notFound();
 	return (
 		<main>
-			<header className=" relative -m-8 md:-m-16 -mt-16 md:-mt-24 mb-8 h-[50vh] ">
-				<Image
-					src={`/works/${work.coverImage}`}
-					width={1920}
-					height={1080}
-					alt={work.title}
-					className=" absolute w-full h-full object-cover  brightness-50"
-				/>
+			<header className=" relative -m-8 md:-m-16 -mt-16 md:-mt-24 mb-8 h-[50vh] md:w-[100vw] md:h-[100vh] ">
+				{isCoverVideo ? (
+					<video
+						src={`/works/${work.coverImage}`}
+						width={1920}
+						height={1080}
+						muted
+						autoPlay
+						loop
+						playsInline
+						controls={false}
+						className=" absolute w-full h-full object-cover brightness-50"
+					/>
+				) : (
+					<Image
+						src={`/works/${work.coverImage}`}
+						width={1920}
+						height={1080}
+						alt={work.title}
+						className=" absolute w-full h-full object-cover  brightness-50"
+					/>
+				)}
+
 				<div className=" absolute top-1/2 left-8 -translate-y-1/2 text-neutral-50 ">
 					<Text variant="h1" className="text-neutral-50">
 						{work.title}
@@ -44,7 +61,9 @@ export default async function BlogPostPage({ params }: { params: Promise<{ slug:
 					</Text>
 				</div>
 			</header>
-			<MDXContent source={work.body} />
+			<FadeInContainer>
+				<MDXContent source={work.body} />
+			</FadeInContainer>
 		</main>
 	);
 }
