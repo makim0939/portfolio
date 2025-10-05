671da66fdbf32713dde9d9bf038a60f871ae8b6c
makim0939 <makim0939@gmail.com>
2025-10-05 09:45:35 +0900

    feat: MdxContentで、カスタムコンポーネントを使うように変更

--- src/components/ui/MdxContent.tsx
+++ src/components/ui/MdxContent.tsx
@@ -1,8 +1,21 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
+import { Text } from "./Text";
 
 export function MDXContent({ source }: { source: string }) {
+	const mdxComponents = {
+		h2: ({ children }: { children: React.ReactNode }) => (
+			<Text variant="h2" className=" tracking-[0.15em] lg:tracking-[0.21em] ">
+				{children}
+			</Text>
+		),
+		p: ({ children }: { children: React.ReactNode }) => (
+			<Text variant="p" className=" tracking-[0.03em] lg:tracking-[0.06em] ">
+				{children}
+			</Text>
+		),
+	};
 	return (
 		<div className="prose prose-neutral max-w-none">
 			<MDXRemote
@@ -13,7 +26,7 @@ export function MDXContent({ source }: { source: string }) {
 						rehypePlugins: [[rehypeSlug], [rehypeAutolinkHeadings, { behavior: "append" }]],
 					},
 				}}
-				components={{}}
+				components={mdxComponents}
 			/>
 		</div>
 	);
